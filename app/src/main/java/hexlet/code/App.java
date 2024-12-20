47876a21b557c4f682b0e3e05f9cb8c135cd62d5
Vladimir Baumgertner <XStationLife@yandex.ru>
2024-12-20 21:04:30 +0300

    add Makefile, update App, update bgk

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,26 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+
+@Command(name = "gendiff",
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Runnable {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        new CommandLine(new App()).execute(args);
+    }
+
+    @Option(names = {"-h", "--help"}, description = "Show this help message and exit.")
+    private Boolean help;
+
+    @Option(names = {"-v", "--version"}, description = "Print version information and exit.")
+    private Boolean version;
+
+    @Override
+    public void run() {
+            CommandLine.usage(this, System.out);
     }
 }
