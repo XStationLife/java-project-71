412073204066d878f1fb39f58dcd598d5056d339
Vladimir Baumgertner <XStationLife@yandex.ru>
2024-12-20 21:44:35 +0300

    update App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,6 +3,10 @@ package hexlet.code;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.io.File;
+import java.util.List;
 
 
 @Command(name = "gendiff",
@@ -13,7 +17,17 @@ public class App implements Runnable {
         new CommandLine(new App()).execute(args);
     }
 
-    @Option(names = {"-h", "--help"}, description = "Show this help message and exit.")
+    @Parameters(description = "path to first file")
+    File filepath1;
+
+    @Parameters(description = "path to second file")
+    File filepath2;
+
+    @Option(names = { "-f", "--format" }, paramLabel = "format", description = "output format [default: stylish]")
+    File archive;
+
+    @Option(names = {"-h", "--help"}, usageHelp = true,
+            description = "Show this help message and exit.")
     private Boolean help;
 
     @Option(names = {"-v", "--version"}, description = "Print version information and exit.")
